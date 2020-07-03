Fix normalization

enhance.py
@@ -48,13 +48,15 @@ class enhance(object):
             self.hdu.verify('silentfix')
             index = 0
             if np.all(self.hdu[index].data == None): index = 1
+            self.norm = np.nanmean(self.hdu[index].data[:,:])
             self.image = np.nan_to_num(self.hdu[index].data[:,:])
             self.header = self.hdu[index].header
             print('Size image: ',self.image.shape)
         else:
             #assume its sunpy map
             #extract data to numpy array
-            self.image = inputFile.data
+            self.norm = np.nanmean(inputFile.data)
+            self.image = np.nan_to_num(inputFile.data)
             #extract header to standard astropy fits header type
             self.header = htf(inputFile.meta)
 
@@ -67,12 +69,12 @@ class enhance(object):
         self.rtype = rtype
         self.big_image = 2048
         self.split = False
-        self.norm = 1.0
+        #self.norm = 1.0
 
-        if self.ntype == 'intensity': 
-            self.norm = np.max(self.image)
-        if self.ntype == 'blos': 
-            self.norm = 1e3
+        #if self.ntype == 'intensity': 
+        #    self.norm = np.max(self.image)
+        #if self.ntype == 'blos': 
+        #    self.norm = 1e3
         
         self.image = self.image/self.norm
 
@@ -155,6 +157,7 @@ class enhance(object):
             self.header['naxis2'] = new_dim[0]
 
         if self.rtype == 'spmap':
+            ktf.clear_session()
             return spmapa(new_data, self.header)
 
         if output != "to_obj":